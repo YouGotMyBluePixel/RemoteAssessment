305009d558cc8faa862ad8c04d21c0d3fc3c7a7c
YouGotMyBluePixel <xaverericketts@gmail.com>
2021-09-21 19:48:46 -0500

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<div id="app">
+   <example-component></example-component>
+</div>
+</html>
+<script src="{{ mix('/js/app.js') }}"></script>
